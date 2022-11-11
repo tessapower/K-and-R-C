fdf6dbd2abfc7579ef7095c432accaa25cc3bb7c
Tessa Power <tessa@tessapower.co>
2022-11-11 16:55:40 +1300

    Add examples from ch 1

---
+++ ch01-intro/examples/symbolic_constants.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define LOWER   0
+#define UPPER 300
+#define STEP   20
+
+int main() {
+  printf(" Celsius | Fahrenheit\n");
+  printf("---------+------------\n");
+  for (float celsius = UPPER; celsius >= LOWER; celsius -= STEP) {
+    printf("%8.0f | %11.1f\n", celsius, (9.0 / 5.0) * (celsius + 32.0));
+  }
+
+  return 0;
+}
