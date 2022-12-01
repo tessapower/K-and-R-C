453aff470dc396992068f98c1f97f41c4a67ec43
Tessa Power <tessa@tessapower.co>
2022-12-02 09:23:56 +1300

    Redo ex 1-6

--- ch01-intro/exercises/Eof/main.c
+++ ch01-intro/exercises/Eof/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 // Exercise 1-6: Verify that the expression `getchar() != EOF` is 0 or 1.
-// Exercise 1-7: Write a program to print the value of `EOF`.
 int main() {
-  printf("(getchar() != EOF) = %d\n", getchar() != EOF);
-  printf("EOF = %d\n", EOF);
+  int c;
+  while (c = getchar() != EOF) {
+    printf("c = %d != EOF\n", c);
+  }
+  printf("c = %d == EOF\n", c);
 
   return 0;
 }
