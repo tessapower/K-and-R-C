42425c045b9830c9cef8135ba7599b3f19e8e231
Tessa Power <tessa@tessapower.co>
2022-12-01 18:10:45 +1300

    Fix style to conform more to standard C style

--- ch01-intro/exercises/Whitespace/main.c
+++ ch01-intro/exercises/Whitespace/main.c
@@ -2,22 +2,20 @@
 
 // Exercise 1-8: Write a program to count blanks, tabs, and newlines.
 int main() {
-  int c;
-  int nl = 0;
-  int t = 0;
   int s = 0;
+  int t = 0;
+  int nl = 0;
 
   printf("> ");
 
+  int c;
   while ((c = getchar()) != EOF) {
-    if (c == ' ') ++s;
-
+    if (c ==  ' ') ++s;
     if (c == '\t') ++t;
-
     if (c == '\n') ++nl;
   }
 
-  printf("Blanks: %d\nTabs: %d\nNewlines: %d\n", s, t, nl);
+  printf("sp: %d\nht: %d\nlf: %d\n", s, t, nl);
 
   return 0;
 }
