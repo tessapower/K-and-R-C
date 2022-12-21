63c120899675d370232f07c9a3e0b9cec9055c58
Tessa Power <tessa@tessapower.co>
2022-12-21 16:44:42 +1300

    Complete exercise 5-9: rewrite date functions with pointers

---
+++ ch05-pointers-and-arrays/exercises/DatePointers/dateutils.c
@@ -0,0 +1,50 @@
+#include "dateutils.h"
+
+#include <stdio.h>
+
+// Exercise 5-9: Rewrite the routines day_of_year and month_day with pointers
+//  instead of indexing.
+
+static char daytable[][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(const int year, const int month, const int day) {
+  // Return error value if the month or year is nonsensical
+  if (month < 1 || month > 12 || year < 1) return -1;
+
+  // Subscript into the correct array of daytable
+  int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+  char *dt = daytable[leap];
+
+  // Return error value if the day for the given month is nonsensical
+  if (day < 1 || day > dt[month]) return -1;
+
+  int d = day;
+  int m = month;
+  while (m--)
+    d += *dt++;
+
+  return d;
+}
+
+void month_day(const int year, const int yearday, int* pmonth, int* pday) {
+  int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+  // Return early if the year or yearday is nonsensical
+  if (year < 1 || yearday < 1 
+    || (leap ? yearday > 366 : yearday > 365))
+    return;
+
+  char *dt = daytable[leap];
+  int d = yearday;
+  int m = 1;
+  while (d > dt[m]) {
+    d -= dt[m++];
+  }
+
+  *pmonth = m;
+  *pday = d;
+}
---
+++ ch05-pointers-and-arrays/exercises/DatePointersTests/DatePointersTests.cpp
@@ -0,0 +1,106 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "dateutils.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DatePointersTests {
+  TEST_CLASS(DatePointersTests) {
+  public:
+    TEST_METHOD(day_of_year_invalidDate) {
+      Assert::AreEqual(-1, day_of_year(0, 0, 0));
+    }
+
+    TEST_METHOD(day_of_year_invalidDay) {
+      Assert::AreEqual(-1, day_of_year(2022, 2, 29));
+      Assert::AreEqual(-1, day_of_year(1984, 5, 59));
+      Assert::AreEqual(-1, day_of_year(1999, 11, 0));
+      Assert::AreEqual(-1, day_of_year(1666, 1, -10));
+    }
+
+    TEST_METHOD(day_of_year_invalidMonth) {
+      Assert::AreEqual(-1, day_of_year(2022, 0, 28));
+      Assert::AreEqual(-1, day_of_year(1984, 13, 19));
+      Assert::AreEqual(-1, day_of_year(1999, 18, 3));
+      Assert::AreEqual(-1, day_of_year(1666, -1, 10));
+    }
+
+    TEST_METHOD(day_of_year_invalidYear) {
+      Assert::AreEqual(-1, day_of_year(0, 1, 28));
+      Assert::AreEqual(-1, day_of_year(-1500, 11, 19));
+    }
+
+    TEST_METHOD(day_of_year_validDate) {
+      Assert::AreEqual(1, day_of_year(2001, 1, 1));
+      Assert::AreEqual(70, day_of_year(1993, 3, 11));
+      Assert::AreEqual(365, day_of_year(2013, 12, 31));
+    }
+
+    TEST_METHOD(day_of_year_leapYear) {
+      Assert::AreEqual(1, day_of_year(2024, 1, 1));
+      Assert::AreEqual(71, day_of_year(1992, 3, 11));
+      Assert::AreEqual(366, day_of_year(1600, 12, 31));
+    }
+
+    TEST_METHOD(month_day_invalidDate) {
+      int day = -1;
+      int month = -1;
+
+      month_day(0, 0, &month, &day);
+
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+    }
+
+    TEST_METHOD(month_day_invalidDay) {
+      int day = -1;
+      int month = -1;
+
+      month_day(1990, 0, &month, &day);
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+
+      month_day(2020, -332, &month, &day);
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+
+      month_day(1909, 366, &month, &day);
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+    }
+
+    TEST_METHOD(month_day_invalidYear) {
+      int day = -1;
+      int month = -1;
+
+      month_day(0, 332, &month, &day);
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+
+      month_day(-1223, 111, &month, &day);
+      Assert::AreEqual(-1, day);
+      Assert::AreEqual(-1, month);
+    }
+
+    TEST_METHOD(month_day_validDate) {
+      int day = -1;
+      int month = -1;
+
+      month_day(1993, 70, &month, &day);
+
+      Assert::AreEqual(11, day);
+      Assert::AreEqual(3, month);
+    }
+
+    TEST_METHOD(month_day_leapYear) {
+      int day = -1;
+      int month = -1;
+
+      month_day(1992, 71, &month, &day);
+
+      Assert::AreEqual(11, day);
+      Assert::AreEqual(3, month);
+    }
+  };
+}
