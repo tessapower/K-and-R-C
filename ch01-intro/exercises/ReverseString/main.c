16301917b00bf9253c29615dd46ff7ff76c9272f
Tessa Power <tessa@tessapower.co>
2023-01-09 08:30:38 +1300

    Add comments and fixes to exercise 1-19

--- ch01-intro/exercises/ReverseString/main.c
+++ ch01-intro/exercises/ReverseString/main.c
@@ -2,8 +2,19 @@
 
 #define MAX_LINE 1000  // Maximum input line size
 
-int getline(char line[], int maxLine);
+/**
+ * @brief Reads a line into s with max length of maxlen, returns the length.
+*/
+int getline(char line[], int maxlen);
+
+/**
+ * @brief Reverses the string s in place.
+*/
 void reverse(char line[]);
+
+/**
+ * @brief Swaps the characters at the indices i and j in s.
+*/
 void swap(char s[], int i, int j);
 
 // Exercise 1-19: Write a function reverse(s) that reverses the character
@@ -16,22 +27,28 @@ int main() {
   while ((linelen = getline(line, MAX_LINE)) > 0) {
     if (line[0] != '\n') {
       reverse(line);
-      printf("%s\n", line);
+      printf("%s", line);
     }
   }
 
   return 0;
 }
 
-// Reads a line into s, returns the length
 int getline(char s[], int lim) {
-  int c;
+  int c = 0;
   // Declare i outside of loop so we can return the length
   int i;
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+  for (i = 0; i < lim - 2 && (c = getchar()) != EOF && c != '\n'; ++i) {
     s[i] = c;
   }
 
+  // Handle end of line
+  if (c == '\n') {
+    // Add final newline char
+    s[i] = c;
+    ++i;
+  }
+  // Add string termination char
   s[i] = '\0';
 
   return i;
@@ -41,6 +58,9 @@ void reverse(char line[]) {
   int len;
   for (len = 0; line[len] != '\0'; ++len)
     ;
+
+  // Leave the newline in place
+  if (line[len - 1] == '\n') len--;
 
   for (int i = 0; i < len / 2; i++) {
     swap(line, i, len - i - 1);
