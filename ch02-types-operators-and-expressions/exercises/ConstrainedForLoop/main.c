f4a73efb19a7fb47bfec93fe913cfe9cfa939f6c
Tessa Power <tessa@tessapower.co>
2023-01-09 10:47:17 +1300

    Add minor fix to exercise 2-2

--- ch02-types-operators-and-expressions/exercises/ConstrainedForLoop/main.c
+++ ch02-types-operators-and-expressions/exercises/ConstrainedForLoop/main.c
@@ -27,10 +27,10 @@ int getline(char s[], const int lim) {
   enum boolean end = FALSE;
   while (!end) {
     if (i >= lim - 1) end = TRUE;
-    else if (!end) {
+    
+    if (!end) {
       char c = getchar();
-      if (c == '\n') end = TRUE;
-      else if (c == EOF) end = TRUE;
+      if (c == '\n' || c == EOF) end = TRUE;
 
       if (!end) s[i++] = c;
     }
