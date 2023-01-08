ba4f23a9577a1ded24b71f85bc8a31c5bbc0944e
Tessa Power <tessa@tessapower.co>
2023-01-09 08:58:25 +1300

    Add comments to exercise 1-21

--- ch01-intro/exercises/Entab/main.c
+++ ch01-intro/exercises/Entab/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
 #define MAX_LINE 80  // Maximum input line size
-#define TAB_WIDTH 8
+#define TAB_INCREMENT 8
 
-int getline(char line[], const int maxLine);
+/**
+ * @brief Reads a line into s with max length of maxlen, returns the length.
+*/
+int getline(char line[], const int maxlen);
+
+/**
+ * @brief Replaces tabs with spaces in s.
+*/
 void entab(char line[]);
+
+/**
+ * @brief Copies the characters from one string to another. The to string
+ *  must be large enough.
+*/
 void copy(char to[], char from[]);
 
-// Exercise 1-21: Write a program entab that replaces strings of blanks by the
+// Exercise 1-21: Write a program entab that replaces strings of spaces by the
 //  minimum number of tabs and blanks to achieve the same spacing. Use the
 //  same tab stops as for detab.
 int main() {
@@ -50,26 +62,26 @@ void entab(char s[]) {
   int j = 0;
 
   int ntabs = 0;
-  int nblanks = 0;
+  int nspaces = 0;
   while (s[i] != '\0' && i < MAX_LINE && j < MAX_LINE - 1) {
-    // Move along the string, copy chars until we hit a blank
+    // Move along the string, copy chars until we hit a space
     if (s[i] == ' ') {
-      // Collect blanks until we get enough to need a tab
-      if (i % TAB_WIDTH == 0) {
-        nblanks = 0;
+      // Collect spaces until we get enough to need a tab
+      if (i % TAB_INCREMENT == 0) {
+        nspaces = 0;
         ntabs++;
       } else {
-        nblanks++;
+        nspaces++;
       }
     } else {
-      // If we have been collecting blanks and tabs, add them before adding
-      // a new character, making sure we don't go over the max line length.
-      // Does nothing if ntabs and nblanks is 0.
+      // If we have been collecting spaces and tabs, add them before the
+      // new character, making sure we don't go over the max line length.
+      // Does nothing if ntabs and nspaces is 0.
       while (ntabs-- > 0 && j < MAX_LINE - 1) tmp[j++] = '\t';
-      while (nblanks-- > 0 && j < MAX_LINE - 1) tmp[j++] = ' ';
+      while (nspaces-- > 0 && j < MAX_LINE - 1) tmp[j++] = ' ';
       // Reset tabs & blanks
       ntabs = 0;
-      nblanks = 0;
+      nspaces = 0;
 
       // Add the character
       tmp[j++] = s[i];
