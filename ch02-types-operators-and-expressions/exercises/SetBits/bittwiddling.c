7f7afa0ce09bbafdff485cbb55547c72cba12ab6
Tessa Power <tessa@tessapower.co>
2023-01-09 14:05:01 +1300

    Convert exercise 2-6 into C lib project

--- ch02-types-operators-and-expressions/exercises/SetBits/bittwiddling.c
+++ ch02-types-operators-and-expressions/exercises/SetBits/bittwiddling.c
@@ -4,6 +4,7 @@
 // Exercise 2-6: Write a function setbits(x, p, n, y) that returns x with the
 //  n bits that begin at position p set to the rightmost n bits of y, 
 //  the other bits unchanged.
+
 unsigned setbits(unsigned x, const unsigned p, const unsigned n, unsigned y) {
   assert(p + 1 - n >= 0);
   // Set n bits at pos p to 0
--- ch02-types-operators-and-expressions/exercises/SetBits/bittwiddling.h
+++ ch02-types-operators-and-expressions/exercises/SetBits/bittwiddling.h
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef BIT_TWIDDLING_H
+#define BIT_TWIDDLING_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 /**
  * @brief Returns x with the n bits that begin at position p set to the rightmost n
  * bits of y, leaving the other bits unchanged.
@@ -10,3 +14,9 @@ unsigned setbits(unsigned x, const unsigned p, const unsigned n, unsigned y);
  * @brief Returns the n lower bits of x.
 */
 unsigned getlowerbits(const unsigned x, const unsigned n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BIT_TWIDDLING_H
