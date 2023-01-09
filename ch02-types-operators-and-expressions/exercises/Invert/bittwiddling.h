d97160d86aa9fe149aeab5f430e0d8034feacfe3
Tessa Power <tessa@tessapower.co>
2023-01-09 14:40:44 +1300

    Convert exercise 2-7 into C lib project

--- ch02-types-operators-and-expressions/exercises/Invert/bittwiddling.h
+++ ch02-types-operators-and-expressions/exercises/Invert/bittwiddling.h
@@ -1,6 +1,17 @@
-#pragma once
+#ifndef BIT_TWIDDLING_H
+#define BIT_TWIDDLING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * @brief Inverts n bits of x starting from position p.
  */
 unsigned invert(unsigned x, const unsigned p, const unsigned n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BIT_TWIDDLING_H
--- ch02-types-operators-and-expressions/exercises/InvertTests/InvertTests.cpp
+++ ch02-types-operators-and-expressions/exercises/InvertTests/InvertTests.cpp
@@ -1,30 +1,29 @@
 #include "gtest/gtest.h"
-extern "C" {
-  #include "bittwiddling.h"
-}
+#include "bittwiddling.h"
+
 
 TEST(invert, invertNothing) {
-  EXPECT_EQ(invert(0b0, 0, 0), 0b0);
+  EXPECT_EQ(invert(0b0, 0, 0),        0b0);
   EXPECT_EQ(invert(0b01010101, 7, 0), 0b01010101);
 }
 
 TEST(invert, invertSingleBit) {
-  EXPECT_EQ(invert(0b0, 0, 1), 0b1);
-  EXPECT_EQ(invert(0b1, 0, 1), 0b0);
+  EXPECT_EQ(invert(0b0, 0, 1),        0b1);
+  EXPECT_EQ(invert(0b1, 0, 1),        0b0);
   EXPECT_EQ(invert(0b01010111, 4, 1), 0b01000111);
   EXPECT_EQ(invert(0b01010101, 3, 1), 0b01011101);
 }
 
 TEST(invert, invertMultipleBits) {
-  EXPECT_EQ(invert(0b0000, 3, 2), 0b1100);
+  EXPECT_EQ(invert(0b0000, 3, 2),     0b1100);
   EXPECT_EQ(invert(0b11100101, 7, 5), 0b00011101);
   EXPECT_EQ(invert(0b01010111, 4, 1), 0b01000111);
   EXPECT_EQ(invert(0b01010101, 3, 1), 0b01011101);
 }
 
 TEST(invert, invertAllBits) {
-  EXPECT_EQ(invert(0b0000, 3, 4), 0b1111);
-  EXPECT_EQ(invert(0b11111111, 7, 8), 0b00000000);
-  EXPECT_EQ(invert(0b11100101, 7, 8), 0b00011010);
+  EXPECT_EQ(invert(0b0000, 3, 4),               0b1111);
+  EXPECT_EQ(invert(0b11111111, 7, 8),           0b00000000);
+  EXPECT_EQ(invert(0b11100101, 7, 8),           0b00011010);
   EXPECT_EQ(invert(0b0110101101010111, 15, 16), 0b1001010010101000);
 }
